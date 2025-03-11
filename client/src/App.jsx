6478b1efd1cf8588ea5fcc317978126d1d260665
Karina1014 <janneth_wi15@hotmail.com>
2025-03-10 19:45:15 -0500

    feat: :sparkles: add sidebar toggle
    add sidebar toggle functionality for mobile view

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,24 @@
 import { useState } from 'react';
 import Header from './components/Header/Header'; // Asegúrate de que la ruta del archivo sea correcta.
+import Sidebar from './components/Sidebar/Sidebar';
 
 function App() {
-  const [darkMode,setDarkMode] = useState(false);
-  
-  const toggleDarkMode=()=>{
-    setDarkMode(!darkMode)
-  }
+  const [darkMode, setDarkMode] = useState(false);
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
   return (
     <div className={`${darkMode ? 'dark' : ''} font-quickSand`}>
-      <Header toggleDarkMode={toggleDarkMode}
-      darkMode={darkMode}/>
+      {/* Pasa la función toggleSidebar como prop */}
+      <Header toggleDarkMode={toggleDarkMode} darkMode={darkMode} toggleSidebar={toggleSidebar} />
+      <Sidebar isSidebarOpen={isSidebarOpen} />
     </div>
   );
 }
